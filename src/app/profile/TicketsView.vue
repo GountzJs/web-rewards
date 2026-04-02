3a2e6e73768c6238952681d3de901554e656d764
Gountzjs <tomasjsdev@gmail.com>
2026-04-02 11:45:51 +0000

    feat: app profile integrate list tickets ticketsview

--- src/app/profile/TicketsView.vue
+++ src/app/profile/TicketsView.vue
@@ -1,7 +1,15 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { useProfileStore } from "@/modules/accounts/store/profile.store.ts";
+import ListTickets from "@/modules/tickets/components/ListTickets.vue";
+import { storeToRefs } from "pinia";
+
+const profile = useProfileStore();
+
+const { data } = storeToRefs(profile);
+</script>
 
 <template>
-  <h1>Tickets</h1>
+  <ListTickets v-if="data" :id="data.id" :username="data.username" :avatar="data.avatar" />
 </template>
 
 <style scoped></style>
